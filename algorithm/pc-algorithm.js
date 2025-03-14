5e2f705d9b3fcd7b5c27119e10efb91d4d0dfc03
DawnProspect <rzaljabar@gmail.com>
2025-03-14 17:06:30 +0700

    Finish few practices, will continue

---
+++ algorithm/pc-algorithm.js
@@ -0,0 +1,58 @@
+
+
+// ? Ingat ini pertanyaan yang dibuat algorithma nya dulu bukan kode ya
+
+// ! NOMOR 1
+// ! Soalnya
+// Input: 7 → Output: "Ganjil"
+// Input: 10 → Output: "Genap"
+
+/*
+Jawabanku
+
+1. Pertama kita cek dulu input yang masuk itu angka berapa
+2. Terus kita ambil angka itu dan kita taro di variable
+3. Terus kita cek apakah angka itu genap atau ganjil
+4. Buat perkondisian kalau angka itu tidak habis dibagi 2 maka console.log ganjil
+5. Buat perkondisian lagi kalau angka itu habis dibagi 2 maka console.log genap
+
+*/
+
+// ? Jawabannnya
+
+/*
+
+1. Terima input: Ambil angka yang ingin dicek
+2. Simpan angka di dalam variable: Simpan angka tersebut di dalam sebuah variable
+3. Buat perkondisian, cek angka genap atau ganjil.
+    Jika angka habis dibagi 2 (input % 2 === 0) berarti genap
+    Jika tidak habis dibagi 2 berarti ganjil
+4. Tampilkan hasilnya dengan cetak console.log "Genap" atau "Ganjil" sesuai dengan hasil pengecekan
+*/
+
+// ! NOMOR DUA
+// Input: "katak" → Output: "Palindrom"
+// Input: "mobil" → Output: "Bukan Palindrom"
+// Input: "radar" → Output: "Palindrom"
+
+/*
+
+1. Pertama kita terima input dan ambil kata berupa string
+2. Kedua simpan kata tersebut di suatu variable
+3. Ketiga buat perkondisian, dengan loop kata yang disimpan
+    Jika kata dicek dan dibalik meenghasilkan kata yang sama dengan inputnya, kita cetak console.log Palindrom
+    Jika kata dicek dan dibalik menghasilkan kata yang berbeda dengan inputnya, kita cetak console.log Bukan Palindrom
+
+*/
+
+// ? Jawabannnya
+
+/*
+
+1. Terima input: Ambil kata yang ingin dicek sebagai string
+2. Simpan kata tersebut dalam suatu variable
+3. Buat variable untuk buat versi terbalik dari kata tersebut
+4. Bandingkan variable yang dibalik dengan kata aslinya, jika sama cetak console.log "Palindrom"
+5. Bandingkan variable yang dibalik dengan kata aslinya, jika berbeda cetak console.log "Bukan palindrome"
+
+*/
