7ceee19f5adf9df27dfa588e71130c53e8d5bf90
DawnProspect <rzaljabar@gmail.com>
2025-04-10 18:28:54 +0700

    Practice small conditional

---
+++ JavaScript/pc-basic-conditional.js
@@ -0,0 +1,40 @@
+
+// STORE kecepatan WITH ANY NUMBER
+// IF kecepatan GREATER THAN 120 THEN
+//     DISPLAY "Bahaya! Kurangi kecepatan!"
+// ELSE IF kecepatan GREATER THAN 90 THEN
+//     DISPLAY "Hati-hati, kecepatan cukup tinggi"
+// ELSE
+//     DISPLAY "Kecepatan aman"
+// END IF
+
+
+// let speed = 122
+
+// if (speed > 120) {
+//     console.log("Bahaya! Kurangi kecepatan!")
+// } else if (speed > 90) {
+//     console.log ("Hati hati, kecepatan cukup tinggi")
+// } else {
+//     console.log ("Kecepatan aman")
+// }
+
+
+// STORE pakaian WITH ANY STRING
+// IF pakaian EQUALS "jas" THEN
+//     DISPLAY "Pakaian sangat formal, cocok untuk presentasi!"
+// ELSE IF pakaian EQUALS "kemeja" THEN
+//     DISPLAY "Pakaian sudah cocok untuk kantor."
+// ELSE
+//     DISPLAY "Pertimbangkan ganti pakaian agar lebih sopan."
+// END IF
+
+let dressCode = "kemeja"
+
+if (dressCode === "jas") {
+    console.log("Pakaian sangat formal, cocok untuk presentasi!")
+} else if (dressCode === "kemeja") {
+    console.log("Pakaian sudah cocok untuk kantor.")
+} else {
+    console.log("Pertimbangkan ganti pakaian agar lebih sopan")
+}
