89cf0c99e961241abc2192ee8b0542bc4f9accfe
DawnProspect <rzaljabar@gmail.com>
2025-04-16 10:12:00 +0700

    add another exercise

--- JavaScript/general/first-test.js
+++ JavaScript/general/first-test.js
@@ -2,8 +2,8 @@ function removeDuplicates(arr) {
     let number = []
     let temp = []
 
-    for(let i = 0; i < arr.length; i++) {
-        if(i === i) {
+    for (let i = 0; i < arr.length; i++) {
+        if (i === i) {
             i += temp
         } else {
             i += number
@@ -11,4 +11,15 @@ function removeDuplicates(arr) {
     }
 }
 removeDuplicates([1, 2, 2, 3, 4, 4, 5]) // [1, 2, 3, 4, 5]
+
+
+function fizzBuzz(n) {
+    // Your code here
+}
+// Output dari 1 sampai n:
+// Jika kelipatan 3 => "Fizz"
+// Jika kelipatan 5 => "Buzz"
+// Jika kelipatan 3 dan 5 => "FizzBuzz"
+// Selain itu tampilkan angkanya
+
 
